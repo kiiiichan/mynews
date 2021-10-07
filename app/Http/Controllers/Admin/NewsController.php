a1d98c6d1f0e0db8bcd60f8fe2ec14f1a60b4a1f
EC2 Default User <ec2-user@ip-172-31-23-140.us-east-2.compute.internal>
2021-10-07 07:14:05 +0000

    誤字修正

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -5,16 +5,16 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
-class ProfileController extends Controller
+class NewsController extends Controller
 {
     //
     public function add()
     {
-        return view('admin.profile.create');
+        return view('admin.news.create');
     }
     
         public function edit()
     {
-        return view('admin.profile.edit');
+        return view('admin.news.edit');
     }
 }
