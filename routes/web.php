de71fded22d320e8b87ce7fa332c157a871f7f16
EC2 Default User <ec2-user@ip-172-31-23-140.us-east-2.compute.internal>
2021-10-13 07:02:00 +0000

    Laravel 16 課題の前まで

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,9 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
      Route::get('news/create', 'Admin\NewsController@add');
      Route::post('news/create', 'Admin\NewsController@create'); 
      Route::get('news', 'Admin\NewsController@index')->middleware('auth');
+     Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth'); // 追記
+     Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth'); // 追記
+     Route::get('news/delete', 'Admin\NewsController@delete')->middleware('auth');
 });
 
 Route::get('XXX',
