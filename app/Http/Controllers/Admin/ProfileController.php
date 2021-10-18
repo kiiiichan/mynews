615cd9be3adeec3c44bd11de6fb65ede4093a6a1
EC2 Default User <ec2-user@ip-172-31-23-140.us-east-2.compute.internal>
2021-10-18 06:58:58 +0000

    Laravel 課題 16

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+// 以下を追記することでProfile Modelが扱えるようになるはず
+use App\Profile;
+
 class ProfileController extends Controller
 {
     //
@@ -13,15 +16,39 @@ class ProfileController extends Controller
         return view('admin.profile.create');
     }
 
-    public function create()
-    {
+      public function create(Request $request)
+  {
+            // 以下を追記 課題16-1
+            
+      // Varidationを行う
+      $this->validate($request, Profile::$rules);
+
+      $profile = new Profile;
+      $form = $request->all();
+
+      // フォームから送信されてきた_tokenを削除する
+      unset($form['_token']);
+
+      // データベースに保存する
+      $profile->fill($form);
+      $profile->save();
         return redirect('admin/profile/create');
     }
 
-    public function edit()
-    {
-        return view('admin.profile.edit');
-    }
+    //public function edit()
+    //{
+    //    return view('admin.profile.edit');
+    //}
+    
+    public function edit(Request $request)
+  {
+      // Profile Modelからデータを取得する
+      $profile = Profile::find($request->id);
+      if (empty($profile)) {
+        abort(404);    
+      }
+      return view('admin.profile.edit', ['profiles_form' => $profile]);
+  }
 
     public function update()
     {
